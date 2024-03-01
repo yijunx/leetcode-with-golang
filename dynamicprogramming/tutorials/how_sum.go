f85a6849567b71d244b26e4d70efa91f8f1512ba
yijun xu <yijun@ucare.io>
2024-03-01 03:00:56 +0000

    finished all how sum

--- dynamicprogramming/tutorials/how_sum.go
+++ dynamicprogramming/tutorials/how_sum.go
@@ -38,9 +38,7 @@ func howSumRecursive(target int, nums []int, memoHow map[int][]int, memoCan map[
 		memoCan[remainder] = new_can
 		// check if we know how to deal with the reminder
 		if new_can {
-			// how_to_add_to_target := append(how_to_add_reminder, num)
-			// fmt.Println("saving", how_to_add_reminder, "to target:", remainder)
-			memoHow[remainder] = new_how
+			// memoHow[remainder] = new_how
 			memoHow[target] = append(new_how, num)
 			return memoHow[target], true
 		}
@@ -52,4 +50,5 @@ func HowSum() {
 	fmt.Println(howSum(23, []int{5, 8}))
 	fmt.Println(howSum(51, []int{2, 4}))
 	fmt.Println(howSum(300, []int{7, 14}))
+	fmt.Println(howSum(280, []int{7, 14}))
 }
